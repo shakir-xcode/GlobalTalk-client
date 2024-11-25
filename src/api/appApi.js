9ee7717409dabe5d1f8ee0e898cafb9d1aef05a8
Shakir <shaker88999@gmail.com>
2024-11-25 11:41:03 +0530

    updated to new baseURI

--- src/api/appApi.js
+++ src/api/appApi.js
@@ -1,4 +1,5 @@
-export const baseURI =  "https://global-talk-server.vercel.app/"
+export const baseURI = "https://global-talk-server-ih9q.vercel.app/";
+//export const baseURI =  "https://global-talk-server.vercel.app/"
 //export const baseURI = "https://globaltalk-server.onrender.com";
 // export const baseURI = "http://192.168.43.250:8080";
 
